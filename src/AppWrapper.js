18bed8325209b0811bc49e6533c9a5c1bb6e13d9
Arjun Singh <singharjun.biz@gmail.com>
2023-01-07 04:10:22 +0530

    Add a wrapper component

---
+++ src/AppWrapper.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Internal dependencies
+import App from "./App";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
+import AddMedicine from "./AddMedicine";
+import ManageMedicines from "./ManageMedicines";
+
+import AppContext from "./AppContext";
+
+import "./index.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <LogIn />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/manage",
+    element: <ManageMedicines />,
+  },
+  {
+    path: "/add",
+    element: <AddMedicine />,
+  },
+]);
+
+export default function AppWrapper() {
+  const [user, setUser] = useState({});
+  const [medicines, setMedicines] = useState([]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+        medicines,
+        setMedicines,
+      }}
+    >
+      <RouterProvider router={router} />
+    </AppContext.Provider>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,46 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Internal dependencies
-import App from "./App";
-import LogIn from "./LogIn";
-import SignUp from "./SignUp";
-import AddMedicine from "./AddMedicine";
-import ManageMedicines from "./ManageMedicines";
-
-import AppContext from "./AppContext";
-
+import AppWrapper from "./AppWrapper";
 import "./index.scss";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <LogIn />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-  {
-    path: "/manage",
-    element: <ManageMedicines />,
-  },
-  {
-    path: "/add",
-    element: <AddMedicine />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppContext.Provider value={{}}>
-      <RouterProvider router={router} />
-    </AppContext.Provider>
+    <AppWrapper />
   </React.StrictMode>
 );

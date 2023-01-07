36bd835ae2a3dd8904423660b2af906a2c879ef6
Shivang Seth <ronakseth2003@gmail.com>
2023-01-07 17:45:14 +0530

    Add Login file to the project

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,3 +1,43 @@
+import Button from "./components/Button"
 export default function LogIn() {
-  return <p>This is the LogIn route</p>;
+  const linkStyle = {
+    color: 'red',
+    textDecoration: 'none'
+  }
+  const styleForm = {
+    height: "100vh",
+    display: 'flex' ,
+    flexDirection: 'column',
+    justifyContent: "center",
+    color: "#c514eb",
+    alignItems: 'center',
+  }
+  return (
+    <div style = { styleForm }>
+      <div>
+      <h1>login</h1>
+      </div>
+      <form>
+
+        <div>
+        <label for="username">USERNAME</label>
+        <br></br>
+        <input type="text" id="username" name="Username"></input>
+        <br></br>
+        </div>
+
+        <div>
+        <label for="password">PASSWORD</label>
+        <br></br> 
+        <input type="password" id="password"></input>
+        <br></br>
+        </div>
+
+      </form>
+      <br></br>
+      <Button>LogIn</Button>
+      <p>Not a user? <a href="" style= { linkStyle }>Sign up</a> instead</p>
+    </div>
+  )
 }
+

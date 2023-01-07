cae16671f4cc4e2a1b3a52d724bb7dbbff85b616
Arjun Singh <singharjun.biz@gmail.com>
2023-01-07 20:58:49 +0530

    Add onClick prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,13 @@
 // Internal dependencies
 import "./Button.scss";
 
-export default function Button({ children, style, className }) {
+export default function Button({ children, style, className, onClick }) {
   return (
-    <button className={`meds-button ${className}`} style={style}>
+    <button
+      className={`meds-button ${className}`}
+      style={style}
+      onClick={onClick}
+    >
       {children}
     </button>
   );

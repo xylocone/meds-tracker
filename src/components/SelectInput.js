a1f4b27da6f7fdd5cba32334625ca4329ef1746d
Arjun Singh <singharjun.biz@gmail.com>
2023-01-07 23:28:21 +0530

    Add label arg to handler

--- src/components/SelectInput.js
+++ src/components/SelectInput.js
@@ -4,7 +4,12 @@ export default function SelectInput({ className, options, onChange }) {
   return (
     <select
       className={`meds-select ${className}`}
-      onChange={(e) => onChange(e.currentTarget.value)}
+      onChange={(e) =>
+        onChange(
+          e.currentTarget.value,
+          e.currentTarget.selectedOptions[0].label
+        )
+      }
       defaultValue="select"
     >
       <option value="select" disabled={true}>

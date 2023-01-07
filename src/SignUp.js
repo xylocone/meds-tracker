8480598e15329914ad46b5122c37d49ea297f075
subhajit1742@gmail.com <subhajit1742@gmail.com>
2023-01-07 17:59:46 +0530

    Create Signup.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,3 +1,43 @@
+import { Form } from "react-router-dom";
+import Button from "./components/Button";
+
+const styleForm = {
+    height: "100vh",
+    display: 'flex' ,
+    flexDirection: 'column',
+    justifyContent: "center",
+    color: "blue" ,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
 export default function SignUp() {
-  return <p>This is the SignUp route</p>;
+  return (
+  <div style={styleForm}>
+    <div>
+      <h1>Sign Up</h1>
+    </div>
+      <Form>
+        <div>
+          <lable for="EMAIL ID">EMAIL ID</lable><br></br>
+          <input type="text" id="EMAIL ID" name="EMAIL ID"></input><br></br>
+        </div>
+
+        <div>
+          <lable for="username">USERNAME</lable><br></br>
+          <input type="text" id="username" name="username"></input><br></br>
+        </div>
+
+        <div>
+          <lable for="password">PASSWORD</lable><br></br>
+          <input type="password" id="password" name="password"></input>
+        </div>      
+      </Form>
+    
+    <br></br>
+    <div>
+      <Button>SignUp</Button>
+    </div>
+  </div>
+  );
 }

518cbaa3a21e5ab607c2622bb5e7e53ad049e0d7
Arjun Singh <singharjun.biz@gmail.com>
2023-01-08 12:38:07 +0530

    Add LogIn route

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,43 +1,60 @@
-import Button from "./components/Button"
-export default function LogIn() {
-  const linkStyle = {
-    color: 'red',
-    textDecoration: 'none'
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Internal dependencies
+import Header from "./components/Header";
+import Input from "./components/Input";
+import Button from "./components/Button";
+
+import AppContext from "./AppContext";
+
+import "./LogIn.scss";
+
+export default function SignUp() {
+  const navigate = useNavigate();
+
+  const { updateUser } = useContext(AppContext);
+
+  const [username, setUsername] = useState();
+  const [pwd, setPwd] = useState();
+
+  async function handleSubmit() {
+    await updateUser({
+      username,
+      pwd,
+    });
+
+    navigate("/");
   }
-  const styleForm = {
-    height: "100vh",
-    display: 'flex' ,
-    flexDirection: 'column',
-    justifyContent: "center",
-    color: "#c514eb",
-    alignItems: 'center',
+
+  function redirectToSignUp() {
+    navigate("/signup");
   }
+
   return (
-    <div style = { styleForm }>
-      <div>
-      <h1>login</h1>
+    <div className="meds-login">
+      <Header heading="Log In" className="meds-login__header" />
+      <div className="meds-login__inputs-wrapper">
+        <Input
+          label="Username"
+          onInput={(e) => setUsername(e.currentTarget.value)}
+        />
+        <Input label="Password" onInput={(e) => setPwd(e.currentTarget)} />
+        <div className="meds-login__buttons-wrapper">
+          <Button
+            className="meds-login__login-button"
+            onClick={() => handleSubmit()}
+          >
+            Log In
+          </Button>
+          <Button
+            className="meds-login__signup-button"
+            onClick={() => redirectToSignUp()}
+          >
+            Sign Up, instead
+          </Button>
+        </div>
       </div>
-      <form>
-
-        <div>
-        <label for="username">USERNAME</label>
-        <br></br>
-        <input type="text" id="username" name="Username"></input>
-        <br></br>
-        </div>
-
-        <div>
-        <label for="password">PASSWORD</label>
-        <br></br> 
-        <input type="password" id="password"></input>
-        <br></br>
-        </div>
-
-      </form>
-      <br></br>
-      <Button>LogIn</Button>
-      <p>Not a user? <a href="" style= { linkStyle }>Sign up</a> instead</p>
     </div>
-  )
+  );
 }
-
